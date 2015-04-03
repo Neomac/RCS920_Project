dda7ae69604918bfbce05b9a6a94d5f244c19c70
Neomac <a.galluet@gmail.com>
2015-04-03 13:47:58 +0200

    Acces to Kinect and get raw joint data

---
+++ KinectTest/KinectTest/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+namespace KinectTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Mode _mode = Mode.Color;
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IList<Body> _bodies;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //System.Diagnostics.Process.Start(@"C:\Windows\System32\Kinect\KinectService.exe");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            //Get a reference to the multi-frame
+            var reference = e.FrameReference.AcquireFrame();
+
+            //Open color frame
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Color)
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }
+                }
+            }
+
+            //Open depth frame
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Depth)
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }
+                }
+            }
+
+            //Open infrared frame
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == Mode.Infrared)
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }
+                }
+            }
+
+            //Output body variables in the console
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    _bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    foreach (var body in _bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+                                IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+
+                                Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                                foreach (JointType jointType in joints.Keys)
+                                {
+                                    Console.WriteLine("Start body");
+                                    Console.WriteLine("Joint position X : {0} \" Joint position Y : {1} \" Joint position Z : {2}", joints[jointType].Position.X, joints[jointType].Position.Y, joints[jointType].Position.Z);
+                                    Console.WriteLine("End body");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private ImageSource ToBitmap(ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            byte[] pixels = new byte[width * height * ((format.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        private ImageSource ToBitmap(DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] pixelData = new ushort[width * height];
+            byte[] pixels = new byte[width * height * (format.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(pixelData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < pixelData.Length; ++depthIndex)
+            {
+                ushort depth = pixelData[depthIndex];
+
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixels[colorIndex++] = intensity; // Blue
+                pixels[colorIndex++] = intensity; // Green
+                pixels[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        private ImageSource ToBitmap(InfraredFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+
+            ushort[] frameData = new ushort[width * height];
+            byte[] pixels = new byte[width * height * (format.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(frameData);
+
+            int colorIndex = 0;
+            for (int infraredIndex = 0; infraredIndex < frameData.Length; infraredIndex++)
+            {
+                ushort ir = frameData[infraredIndex];
+
+                byte intensity = (byte)(ir >> 7);
+
+                pixels[colorIndex++] = (byte)(intensity / 1); // Blue
+                pixels[colorIndex++] = (byte)(intensity / 1); // Green   
+                pixels[colorIndex++] = (byte)(intensity / 0.4); // Red
+
+                colorIndex++;
+            }
+
+            int stride = width * format.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = Mode.Infrared;
+        }
+    }
+
+    public enum Mode
+    {
+        Color,
+        Depth,
+        Infrared
+    }
+}
